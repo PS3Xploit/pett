9585858fe7adb7cd1254c5e619fcbe7282e77e0b
PS3Xploit Team <33543391+PS3Xploit@users.noreply.github.com>
2018-03-12 22:30:11 -0400

    Tweaked Search

--- files/js/chains/loader.js
+++ files/js/chains/loader.js
@@ -37,6 +37,7 @@ function initROP()
 		// Find File Pointers
 		showFoundOffsets(offset_find_base_fp);
 		if((!base_found)&&(!stackframe_found)&&(!j2_found)&&(!j1_found)){base_found=findBase();}
+		//if((base_found)&&(base_fp_addr!=0)){base_fp_addr=base_offsets[0];}
 		//if(!base_found){findBase();}
 		//if(!base_found){reloadInitROP();}
 		showFoundOffsets(offset_find_base_fp);
@@ -44,6 +45,7 @@ function initROP()
 		// Find Stackframe Pointer
 		showFoundOffsets(offset_find_stack_frame);
 		if((base_found)&&(!stackframe_found)&&(!j2_found)&&(!j1_found)){stackframe_found=findStackFrame();}
+		//if((stackframe_found)&&(stack_frame_addr!=0)){stack_frame_addr=stack_offsets[0];}
 		//if(!stackframe_found){findStackFrame();}
 		//if(!stackframe_found){reloadInitROP();}
 		showFoundOffsets(offset_find_stack_frame);
@@ -51,6 +53,8 @@ function initROP()
 		// Find Jump 2 Pointer
 		showFoundOffsets(offset_find_jump2);
 		if((base_found)&&(stackframe_found)&&(!j2_found)&&(!j1_found)){j2_found=findJump2();}
+		if((base_found)&&(stackframe_found)&&(!j2_found)&&(j1_found)){j2_found=findJump2();}
+		//if((j2_found)&&(jump_2_addr!=0)){jump_2_addr=jump2_offsets[0];}
 		//if(!j2_found){findJump2();}
 		//if(!j2_found){reloadInitROP();}
 		showFoundOffsets(offset_find_jump2);
@@ -58,6 +62,7 @@ function initROP()
 		// Find Jump 1 Pointer
 		showFoundOffsets(offset_find_jump1);
 		if((base_found)&&(stackframe_found)&&(j2_found)&&(!j1_found)){j1_found=findJump1();}
+		//if((j1_found)&&(jump_1_addr!=0)){jump_1_addr=jump1_offsets[0];}
 		//if(!j1_found){findJump1();}
 		//if(!j1_found){reloadInitROP();}
 		showFoundOffsets(offset_find_jump1);
